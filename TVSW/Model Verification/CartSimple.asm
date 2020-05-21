fffd031bafae12d566f84c488c5a60cf78496348
Samuele Ferri <samuelexferri@gmail.com>
2020-05-21 16:52:41 +0200

    Update

--- TVSW/Model Verification/CartSimple.asm
+++ TVSW/Model Verification/CartSimple.asm
@@ -1,11 +1,12 @@
-asm Cart
+asm CartSimple // No output message, Drug not abstract domain but enum domain (Eliminate static variables), Fixed price for generic drugs
 
 import ./STDL/StandardLibrary
+import ./STDL/CTLlibrary
 
 signature:
 
 	// DOMAINS
-	abstract domain Drug
+	enum domain Drug = {LITIO | MORFINA | PARACETAMOLO}
 	domain SubInteger subsetof Integer
 	enum domain States = {WAITING | ADD_PRODUCT_OR_EXIT | CHOOSE_GEN_COM | SELECTED_GENERIC | SELECTED_COMMERCIAL | CLOSED}
 	enum domain Actions = {ORDER | EXIT}
@@ -15,10 +16,9 @@ signature:
 	// CONTROLLED VARAIBLES
 	dynamic controlled cartState: States
 	dynamic controlled currentDrug: Drug	
-	dynamic controlled outMess: Any	// Output message
-	dynamic controlled total: Integer
-	dynamic controlled numOfProductsInCart: Integer
-
+	dynamic controlled total: SubInteger
+	dynamic controlled numOfProductsInCart: SubInteger
+	
 	// MONITORED VARAIBLES
 	dynamic monitored action: Actions 
 	dynamic monitored selectedDrug: Drug 
@@ -26,17 +26,9 @@ signature:
 	dynamic monitored selectedDrugType: SelectType
 	dynamic monitored insertQuantity: SubInteger
 	dynamic monitored insertPrice: SubInteger
-	
-	// STATIC VARIABLES
-	static litio: Drug
-	static morfina: Drug
-	static paracetamolo: Drug
-	
+
 	// DERIVED FUNCTION
 	derived valid: Boolean
-
-	// STATIC FUNCTION (N-ARIE)
-	static getPrice: Drug -> Integer  
 	
 definitions:
 	// DOMAIN DEFINITIONS
@@ -45,26 +37,12 @@ definitions:
 	// DERIVED FUNCTION
 	function valid = numOfProductsInCart < 2 // Max number of products in cart
 	
-	// STATIC FUNCTION (N-ARIE)
-	function getPrice($c in Drug) = // $c Input a string
-		switch($c)
-			case paracetamolo : 4
-			case morfina : 10
-			case litio : 7
-		endswitch
-
 	// MACRO RULE SUPPORT
 	macro rule r_AddGenericToTotal =
-		seq
-		total := total + getPrice(currentDrug)*insertQuantity
-		outMess := "Total price updated!"	
-		endseq
+		total := total + 1*insertQuantity // There is a problem [plus or mult(UndefValue,IntegerValue)] so i fixed 1 the price
 	
 	macro rule r_AddCommercialToTotal =
-		seq
 		total := total + insertPrice*insertQuantity
-		outMess := "Total price updated!"	
-		endseq
 	
 	// MACRO RULE MAIN
 	macro rule r_Waiting =
@@ -72,15 +50,16 @@ definitions:
 			par
 			if (action=EXIT) then
 				par
+				numOfProductsInCart := 0 // Necessary
 				cartState := CLOSED
-				outMess := "Payment successful!"
 				endpar
 			endif
 			
 			if (action=ORDER) then
 				par
+				total := 0
+				numOfProductsInCart := 0
 				cartState := ADD_PRODUCT_OR_EXIT
-				outMess := "Select a generic or commercial drug"
 				endpar
 			endif
 			endpar
@@ -88,65 +67,56 @@ definitions:
 	
 	macro rule r_SelectAddProductOrExit =
 		if (cartState=ADD_PRODUCT_OR_EXIT) then
-			seq
+			par
 			if (selectedAddProduct=YES) then
-				par
 				cartState := CHOOSE_GEN_COM
-				outMess := "Choose the type of drugs (generic or commercial)"
-				endpar
 			endif
 			
 			if (selectedAddProduct=NO) then
-				par
-				outMess := "Back"
 				cartState := WAITING
-				endpar
 			endif
-			endseq
+			endpar
 		endif
 
 	macro rule r_SelectDrugType =
 		if (cartState=CHOOSE_GEN_COM) then
 			par
 			if (selectedDrugType=GENERIC) then
-				par 
 				cartState := SELECTED_GENERIC
-				outMess := "Insert a name of a generic drug in the list"
-				endpar
 			endif
 			
 			if (selectedDrugType=COMMERCIAL) then
-				par 
 				cartState := SELECTED_COMMERCIAL
-				outMess := "Insert a name of a new commercial drug"
-				endpar
 			endif
 			endpar
 		endif
 	
-	macro rule r_GenericDrugSelected =
+	macro rule r_DrugDetail =
+		par
 		if(cartState=SELECTED_GENERIC) then
-			if ((exist $c in Drug with $c=selectedDrug)) then	
-				seq
-				currentDrug := selectedDrug
-				r_AddGenericToTotal[]
-				numOfProductsInCart := numOfProductsInCart + 1
-				cartState := ADD_PRODUCT_OR_EXIT
-				endseq
-			endif
+			par
+			r_AddGenericToTotal[]
+			numOfProductsInCart := numOfProductsInCart + 1
+			cartState := ADD_PRODUCT_OR_EXIT
+			endpar
 		endif
-	
-	macro rule r_CommercialDrugSelected =
+		
 		if (cartState=SELECTED_COMMERCIAL) then
-			seq
+			par
 			r_AddCommercialToTotal[]
 			numOfProductsInCart := numOfProductsInCart + 1
 			cartState := ADD_PRODUCT_OR_EXIT
-			endseq
+			endpar
 		endif
+		endpar
 		
 	macro rule r_CheckoutForced = 
 		cartState := CLOSED
+
+	// CTL
+	// TODO
+	// I prodotti nel carrell sono sempre minori o uguali di due
+	//CTLSPEC ag(numOfProductsInCart <= 2)
 
 	// MAIN RULE
 	main rule r_Main =
@@ -156,8 +126,7 @@ definitions:
 				par
 				r_SelectAddProductOrExit[]
 				r_SelectDrugType[]
-				r_GenericDrugSelected[]
-				r_CommercialDrugSelected[]
+				r_DrugDetail[]
 				endpar
 			else
 				r_CheckoutForced[]
@@ -169,3 +138,8 @@ default init s0:
 	function cartState = WAITING
 	function numOfProductsInCart = 0 
 	function total = 0 
+
+// TODO:
+// Max product nel carrello (AG)
+// Simulare che prima non c'era SubInteger nel Model Advisor o che comprendeva anche lo 0
+// else skip
